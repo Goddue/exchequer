4c44bf646c9aa32111ac2a77948f49b80afb2b5e
Goddue <goddues@yandex.ru>
2026-04-18 00:24:07 +0300

    Add balance, income expence cards

--- exchequer/src/App.js
+++ exchequer/src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+       <div className="summary">
+        <div className="card income">Доход: {} ₽</div>
+        <div className="card expense">Расход: {} ₽</div>
+        <div className="card balance">Баланс: {} ₽
+        </div>
+      </div>
     </div>
   );
 }
