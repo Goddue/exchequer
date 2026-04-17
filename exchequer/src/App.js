8bdf359f77254a02c68d8ea2804fdece88aa6cc8
Goddue <goddues@yandex.ru>
2026-04-18 00:30:52 +0300

    Add operation adding form

--- exchequer/src/App.js
+++ exchequer/src/App.js
@@ -9,6 +9,35 @@ function App() {
         <div className="card balance">Баланс: {} ₽
         </div>
       </div>
+
+      <form className="add_form">
+        <div className="form-group">
+          <label>Тип:</label>
+          <select>
+            <option value="expense">Расход</option>
+            <option value="income">Доход</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Категория:</label>
+          <input
+            type="text"
+            placeholder="еда, зарплата..."
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Сумма:</label>
+          <input
+            type="number"
+            step="0.01"
+            min="0.01"
+            placeholder="0.00"
+            required
+          />
+        </div>
+        <button type="submit">Добавить</button>   {}
+      </form>
     </div>
   );
 }
