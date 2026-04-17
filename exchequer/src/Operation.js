f33737dffe8686cc946fb1f577ca20d5e0378344
Goddue <goddues@yandex.ru>
2026-04-18 00:37:00 +0300

    Add operation card function

---
+++ exchequer/src/Operation.js
@@ -0,0 +1,15 @@
+ 
+
+function OperationCard() {
+    return (
+        <div className={'operation_card'}>
+            <div className="card_details">
+                <div className="card-category"></div>
+                <div className="card-type"></div>
+            </div>
+            <div className="card-amount"></div>
+        </div>
+    );
+};
+
+export default OperationCard;
